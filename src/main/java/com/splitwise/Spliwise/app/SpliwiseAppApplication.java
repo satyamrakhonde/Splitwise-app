d2d6aba12a9fcddb5c1b74cbe239a5c582f1da9d
satyamrakhonde <satyamrakhonde@gmail.com>
2025-10-03 13:30:07 +0530

    initial setup

---
+++ src/main/java/com/splitwise/Spliwise/app/SpliwiseAppApplication.java
@@ -0,0 +1,13 @@
+package com.splitwise.Spliwise.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpliwiseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpliwiseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitwise/Spliwise/app/SpliwiseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwise.Spliwise.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpliwiseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
